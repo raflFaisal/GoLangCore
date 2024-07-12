12f676e2e164399bfd85e4775e7479c53296600b
Faisal Rafiuddin <faisal.rafiuddin@booking.com>
2024-07-12 23:51:46 +0200

    minor refactor

--- config/env.go
+++ config/env.go
@@ -11,22 +11,6 @@ var (
 	Port = getEnvInt("SERVER_HTTP_PORT", 9194)
 )
 
-func getEnvInt(key string, fallback int) int {
-	k, ok := os.LookupEnv(key)
-
-	if ok && k != "" {
-		val, err := strconv.Atoi(k)
-		if err != nil {
-			errMsg := fmt.Errorf("env var key '%s': '%s' defaulting to '%d' error: %s", key, k, fallback, err.Error())
-			log.Error("failed to parse env var using default", slog.Any("error", errMsg))
-			return fallback
-		}
-		return val
-	}
-
-	return fallback
-}
-
 func getEnvStr(key string, fallback string) string {
 	k, ok := os.LookupEnv(key)
 	if ok && k != "" {
@@ -49,4 +33,20 @@ func getEnvBoolean(key string, fallback bool) bool {
 	}
 
 	return booleanValue
+}
+
+func getEnvInt(key string, fallback int) int {
+	k, ok := os.LookupEnv(key)
+
+	if ok && k != "" {
+		val, err := strconv.Atoi(k)
+		if err != nil {
+			errMsg := fmt.Errorf("env var key '%s': '%s' defaulting to '%d' error: %s", key, k, fallback, err.Error())
+			log.Error("failed to parse env var using default", slog.Any("error", errMsg))
+			return fallback
+		}
+		return val
+	}
+
+	return fallback
 }
